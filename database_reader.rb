77ba7ed6f85ad29e7888c426220384ba1d83fb16
katherineabuhadal <katherine8@verizon.net>
2014-07-16 12:00:07 -0400

    read csv and print hotel names

---
+++ database_reader.rb
@@ -0,0 +1,16 @@
+require "CSV"
+
+class DatabaseReader 
+  def initialize(file_name)
+    @file_name = file_name
+  end
+
+  def read
+    CSV.foreach(@file_name, {:headers => true}) do |row|
+      puts row["Hotel"]
+    end
+  end
+end
+
+database_reader = DatabaseReader.new("hotels.csv")
+database_reader.read
