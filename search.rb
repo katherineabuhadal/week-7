b94b3975bcf182cdcec88068041b94b168ea2d63
katherineabuhadal <katherine8@verizon.net>
2014-07-17 07:06:21 -0400

    make new folder for hotel

---
+++ search.rb
@@ -0,0 +1,26 @@
+require "./database_reader"
+
+class Search
+  def initialize(hotels)
+    @hotels = hotels
+  end
+
+  def conduct
+    prompt_user
+    check_for_hotel
+  end
+
+  def prompt_user
+    print "What is the hotel name?"
+    @user_query = gets.chomp
+  end
+
+  def check_for_hotel
+    hotel =  @hotels.find { |hotel| hotel.name == @user_query } 
+    puts hotel.display
+  end
+end
+
+database_reader = DatabaseReader.new("hotels.csv")
+search = Search.new(database_reader.hotels)
+search.conduct
